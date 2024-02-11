7dd143001e016af0315de2764720c5581d74fbd7
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-11 21:01:06 +0100

    Update MarketStall.cs

--- Silly Market Idle/Assets/Scripts/MarketStall.cs
+++ Silly Market Idle/Assets/Scripts/MarketStall.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MarketStall : MonoBehaviour
 {
+    [SerializeField]
+    private int level = 1;
+    [SerializeField]
+    private float baseEarningRate = 1.0f;
+    [SerializeField]
+    private float earningRate, upgradeCost;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        earningRate = baseEarningRate;
+        upgradeCost = (10 * level) * 1.1f ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Upgrading, Earning, Selling, Downgrading ? 
     }
 }
