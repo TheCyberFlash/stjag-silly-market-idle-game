e4c0ca7864026fe58cc18fe374494d6cba9a729d
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-11 20:57:47 +0100

    Update GameManager.cs

--- Silly Market Idle/Assets/Scripts/GameManager.cs
+++ Silly Market Idle/Assets/Scripts/GameManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
